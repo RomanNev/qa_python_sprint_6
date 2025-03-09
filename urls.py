60b2f6daaca6f58ec66978c856c6a0c69e083fc1
RomanGolub <romannovoselovcl@gmail.com>
2025-03-09 23:50:05 +0300

    Реализована первая версия проекта автотестов для сайта 'Самокат'. - Настроена структура проекта с использованием Page Object Model (POM). - Реализованы тесты для FAQ, оформления заказа и навигации по логотипам. - Добавлена параметризация для тестов FAQ и заказа. - Подключены Allure-отчеты с шагами, заголовками и описаниями. - Написан README с инструкциями по запуску и особенностями. Готово для ревью.

---
+++ urls.py
@@ -0,0 +1,5 @@
+class Urls:
+    main_page_url = 'https://qa-scooter.praktikum-services.ru/'
+    order_page_url = 'https://qa-scooter.praktikum-services.ru/order'
+    redirect_dzen_url = 'https://dzen.ru/?yredirect=true'
+
